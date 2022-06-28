00f424982172113f77307a4ecbf69b24ddbb6e80
Vinay Gupta <Vinay.dhyutidhara@gmail.com>
2022-06-28 22:29:09 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import About from "./partials/About";
-import Card from "./partials/Card";
-import Footer from "./partials/Footer";
-import Projects from "./partials/Projects";
-import Skills from "./partials/Skills";
-import data from "./assets/data";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  useEffect(() => {
-    AOS.init({
-      once: true,
-    });
-  });
   return (
-    <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
-      <div data-aos="fade-down" data-aos-duration="800">
-        <Card name={data.name} title={data.title} social={data.social} />
-      </div>
-      <div data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
-        <About title={data.about.title} description={data.about.description} />
-        <Skills skills={data.skills} />
-        <Projects projects={data.projects} />
-        <Footer github={data.social.github} />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
 export default App;

46e34981dbf0b4289ead7c6864c02bdd10f60df8
Vinay Gupta <Vinay.dhyutidhara@gmail.com>
2023-02-19 13:45:14 +0530

    Updated Registrartion form based on react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setState((prevProps) => ({ ...prevProps, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(state);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,12 +33,20 @@ function App() {
       </header>
       <h1>Registration Form</h1>
       <p>Please fill out this form with the required information</p>
-      <form action='https://register-demo.freecodecamp.org'>
+      <form action='https://register-demo.freecodecamp.org' onSubmit={handleSubmit}>
         <fieldset>
-          <label for="first-name">Enter Your First Name: <input id="first-name" name="first-name" type="text" required /></label>
-          <label for="last-name">Enter Your Last Name: <input id="last-name" name="last-name" type="text" required /></label>
-          <label for="email">Enter Your Email: <input id="email" name="email" type="email" required /></label>
-          <label for="new-password">Enter Your Password: <input id="new-password" name="new-password" type="password" pattern="[a-z0-5]{8,}" required /></label>
+          <label htmlFor="first-name">Enter Your First Name:
+            <input id="first-name" name="first-name" type="text" value={state.firstName} onChange={handleInputChange} required />
+          </label>
+          <label htmlFor="last-name">Enter Your Last Name:
+            <input id="last-name" name="last-name" type="text" value={state.lastName} onChange={handleInputChange} required />
+          </label>
+          <label htmlFor="email">Enter Your Email:
+            <input id="email" name="email" type="email" value={state.email} onChange={handleInputChange} required />
+          </label>
+          <label htmlFor="new-password">Enter Your Password:
+            <input id="new-password" name="new-password" type="password" pattern="[a-z0-5]{8,}" value={state.password} onChange={handleInputChange} required />
+          </label>
         </fieldset>
         <input type="submit" value="Submit" />
       </form>
